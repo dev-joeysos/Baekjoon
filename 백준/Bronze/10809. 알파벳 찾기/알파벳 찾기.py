6f94b43f720f75958ffae69e6e4b4f795f639181
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2023-03-16 12:55:27 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,13 +1,4 @@
-import string
-S = list(input())
-al = list(string.ascii_lowercase)
+S = input()
 
-for i in range(len(S)):
-    for j in range(len(al)):
-        if S[i] == al[j]:
-            al[j] = i
-
-for i in range(len(al)):
-    if not str(al[i]).isdigit():
-        al[i] = -1
-print(*al)
+for x in 'abcdefghijklmnopqrstuvwxyz':
+    print(S.find(x), end = ' ')
