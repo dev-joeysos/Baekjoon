843bf95b3eb2d07d4d038471f7574018bed42383
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2023-07-30 00:01:25 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,15 +1,8 @@
 N, B = input().split()
 B = int(B)
 result = 0
-power = 0
 
-for item in N[::-1]:
-    if item.isdigit():
-        item = int(item)
-    else:
-        item = item.upper()
-        item = ord(item) - ord('A') + 10  # A=10, B=11, ..., Z=35로 변환
-    result += item * (B ** power)
-    power += 1
+for char in N:
+    result = B * result + (int(char) if char.isdigit() else (10 + ord(char.upper()) - ord('A')))
 
 print(result)
