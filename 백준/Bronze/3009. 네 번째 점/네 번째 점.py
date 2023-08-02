c772b5502048f82bfca1bc5f1c6316bb606641a2
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2023-08-02 23:05:00 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,17 @@
+# 1 일 1 백준
+# 230802
+# count() 활용하기
+x_list = []
+y_list = []
+for _ in range(3):
+    x, y = map(int, input().split())
+    x_list.append(x)
+    y_list.append(y)
+
+for i in range(3):
+    if x_list.count(x_list[i]) == 1:
+        x4 = x_list[i]
+    if y_list.count(y_list[i]) == 1:
+        y4 = y_list[i]
+
+print(x4, y4)
