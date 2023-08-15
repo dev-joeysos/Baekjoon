a8103b390a81c2f44df09803e420c8234cb15bb8
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2023-08-15 21:21:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 736 ms, Memory: 138540 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -3,14 +3,16 @@ N_lst = list(map(int, input().split()))
 M = int(input())
 M_lst = list(map(int, input().split()))
 
-# 키: M_lst에 있는 숫자, 값: 그 숫자가 M_lst에 나타나는 횟수
-count_dict = {num: 0 for num in M_lst}
+dict = {}
+for num in M_lst:
+    dict[num] = 0
 
-# N_lst에 있는 숫자를 세기
+count = []
 for num in N_lst:
-    if num in count_dict:
-        count_dict[num] += 1
+    if num in dict:
+        dict[num] += 1
 
-# 결과 출력
-count = [count_dict[num] for num in M_lst]
+for num in M_lst:
+    count.append(dict[num])
+
 print(' '.join(map(str, count)))
