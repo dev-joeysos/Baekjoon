989c4d11369e30eb00b9b019bef12a50a507780a
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2023-08-17 12:35:30 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 2652 ms, Memory: 41628 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,26 +1,30 @@
 import math
 import sys
 
-MAX_N = 1000000  # 예시로 1,000,000을 최대값으로 설정. 문제의 제한에 따라 변경 가능
+MAX_N = 1000000
+
 
 def is_prime(N):
     arr = [True for _ in range(N+1)]
     arr[0], arr[1] = False, False
     for i in range(2, int(math.sqrt(N)+1)):
-        if arr[i]:
-            for j in range(i*i, N+1, i):
-                arr[j] = False
+        if arr[i] == True:
+            j = 2
+            while i*j <= N:
+                arr[i*j] = False
+                j += 1
     return arr
 
-prime_list = is_prime(MAX_N)
 
+# 미리 제한 범위의 소수 리스트를 만든다
+# N을 2로 나눈 소수까지만 합의 여부를 확인하면 된다
+# 해당 합 조건을 만족하는 소수가 존재하면 count +1
 T = int(input())
+prime_list = is_prime(MAX_N)
 for _ in range(T):
     N = int(sys.stdin.readline())
     count = 0
-
     for num in range(2, N//2 + 1):
         if prime_list[num] and prime_list[N - num]:
             count += 1
-
     print(count)
