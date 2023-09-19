d5c672084a8569cd562ae3fd572711753111acee
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2023-09-19 13:11:44 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 584 ms, Memory: 66628 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -2,32 +2,27 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
-N, M, R = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-path = []
-result = [0]*(N+1)
-visited = [-1]*(N+1)
+def dfs(node):
+    visited[node] = True
+    path.append(node)
+    for next_node in graph[node]:
+        if not visited[next_node]:
+            dfs(next_node)
+    return
 
-for _ in range(M):
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+path = []
+result = [0]*(n+1)
+for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-
 for i in range(1, len(graph)):
     graph[i].sort()
-
-def DFS(start):
-    visited[start] = 1
-    path.append(start)
-    
-    for adj_node in graph[start]:
-        if visited[adj_node] == -1:
-            DFS(adj_node)
-    
-    return
-
-DFS(R)
-
+dfs(r)
 
 for idx, node in enumerate(path, start=1):
     result[node] = idx
