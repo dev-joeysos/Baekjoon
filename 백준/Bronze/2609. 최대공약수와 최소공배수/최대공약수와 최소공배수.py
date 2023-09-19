40c2bc83e2295a5accba3e0133d577de396b4041
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2023-09-20 00:27:24 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 252 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -3,28 +3,14 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-# 약수 찾기
-s = min(n, m)
-a1 = []
-a2 = []
-for i in range(1, s+1):
-    tmp1 = n / i
-    tmp2 = m / i
+# 최대공약수
+for i in range(min(n, m), 0, -1):
+    if n % i == 0 and m % i == 0:
+        print(i)
+        break
 
-    if int(tmp1) == tmp1:
-        a1.append(i)
-        a1.append(int(tmp1))
-    if int(tmp2) == tmp2:
-        a2.append(i)
-        a2.append(int(tmp2))
-
-ans = []
-for item in a1:
-    if item in a2:
-        ans.append(item)
-
-S = max(ans)
-print(S)
-
-rst = (n*m)//S
-print(rst)
+# 최소공배수
+for i in range(max(n, m), n*m + 1):
+    if i % n == 0 and i % m == 0:
+        print(i)
+        break
