566cab14b5e1c809be3777fa1cb1b3b4df240f15
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2023-10-06 15:07:36 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
@@ -1,9 +1,3 @@
 def solution(n_str):
-    answer = ''
-    for i in range(len(n_str)):
-        if n_str[i] == '0':
-            continue
-        else:
-            return n_str[i:]
-        
-  
+    return str(int(n_str))
+    # 와 리얼 미친놈인가ㅋㅋ
