7b4043f50e6b60333f96193932544d8a798449b0
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2023-10-06 14:56:22 +0900

    [unrated] Title: l로 만들기, Time: 7.89 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -1,8 +1,4 @@
 def solution(s):
-    answer = ''
-    for i in s:
-        if ord(i) < ord('l'):
-            answer += 'l'
-        else:
-            answer += i 
-    return answer
+    return ''.join('l' if ord(i)<ord('l') else i for i in s )
+
+# 파이썬 문자열 간의 단순 비교도 지원하기는 함
