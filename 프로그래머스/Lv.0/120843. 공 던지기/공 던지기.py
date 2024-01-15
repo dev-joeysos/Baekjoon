809e7b29f384feeebfcdf06326d9ca38d2efd519
Joeysos <77220829+dev-joeysos@users.noreply.github.com>
2024-01-15 18:49:23 +0900

    [level Lv.0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/Lv.0/120843. 공 던지기/공 던지기.py
@@ -1,5 +1,2 @@
 def solution(numbers, k):
-    if 2*(k-1) >= len(numbers):
-        return numbers[2*(k-1) % len(numbers)]
-    else:
-        return numbers[2*(k-1)]
+    return numbers[2*(k-1) % len(numbers)]
