6e556744b5efb91de1392ad659708829963ef208
joeysos <77220829+dev-joeysos@users.noreply.github.com>
2024-10-03 23:08:55 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,21 +1,13 @@
-# great common divisor
 def gcd(a, b): 
-    val = 1
-    for i in range(1, min(a, b) + 1):
-        if a % i == 0 and b % i == 0:
-            val = i
-    return val
+    while b:
+        a, b = b, a % b
+    return a
 
-# least common multiplier
 def lcm(a, b):
     return a * b // gcd(a, b)
 
 def solution(arr):
     temp = 1
-    
     for i in range(len(arr)):
         temp = lcm(temp, arr[i])
-        print(f'temp = {temp}')
     return temp
-
-print(solution([2,6,8,14]))
