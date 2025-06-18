d510ed5b53e823705b75d9ed66d86285f4a5b032
joeysos <joeysos@naver.com>
2025-06-19 00:26:32 +0900

    [Gold V] Title: 최소비용 구하기, Time: 228 ms, Memory: 122964 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,33 +1,40 @@
-import sys
-import heapq
-
-input = sys.stdin.readline
-n = int(input())
-m = int(input())
-
-graph = [[] for _ in range(n + 1)]
+'''
+다익스트라=최소거리가 노드 수 만큼 필요
++우선순위 큐
++직전 노드의 기록이 노드 수 만큼 필요(경로 복원할 때)
+'''
+import sys,heapq
+input=sys.stdin.readline
+n=int(input())
+m=int(input())
+graph=[[] for _ in range(n+1)]
 for _ in range(m):
-    u, v, w = map(int, input().split())
-    graph[u].append((v, w))
+    cn,nn,cost=map(int,input().split())
+    graph[cn].append((nn,cost)) #다음 노드, 비용
 
-start, end = map(int, input().split())
-INF = float('inf')
-dist = [INF] * (n + 1)
-dist[start] = 0
+start,end=map(int,input().split())
+INF=float('inf')
+dist=[INF]*(n+1) #최소거리저장
+dist[start]=0
+#prev=[0]*(n+1) #직전노드저장
+#print(*graph)
 
-heap = []
-heapq.heappush(heap, (0, start))  # (거리, 노드)
+heap=[] #우선순위 큐
+heapq.heappush(heap,(0,start)) #거리, 노드 번호
 
 while heap:
-    cost, now = heapq.heappop(heap)
-    
-    if dist[now] < cost:
+    #print(*heap)
+    cost,current_node=heapq.heappop(heap)
+    #print(f'cost={cost}, current_node={current_node}')
+    if dist[current_node]<cost:
         continue
-
-    for next_node, weight in graph[now]:
-        new_cost = cost + weight
-        if new_cost < dist[next_node]:
-            dist[next_node] = new_cost
-            heapq.heappush(heap, (new_cost, next_node))
+    
+    for next_node,weight in graph[current_node]:
+        new_cost=cost+weight
+        # 현재 다음 노드까지 갈 수 있는 최소 비용 > 새로운 노드로 향하는 비용
+        if dist[next_node]>new_cost:
+            dist[next_node]=new_cost
+            # prev[next_node]=current_node 경로 복원시 사용
+            heapq.heappush(heap,(new_cost,next_node))
 
 print(dist[end])
